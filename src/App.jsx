37dc284c115935445e815f79a79d3ea0ef707714
abr-ya <gamer05@list.ru>
2020-12-27 13:54:49 +0300

    Add Start and Stop game

--- src/App.jsx
+++ src/App.jsx
@@ -15,16 +15,21 @@ const App = () => {
   const [gameOver, setGameOver] = useState(false);
 
   const startGame = () => {
-
+    setSnake(SNAKE_START);
+    setApple(APPLE_START);
+    setDir([0, -1]);
+    setSpeed(SPEED);
+    setGameOver(false);
   };
 
   const endGame = () => {
-
+    setSpeed(null);
+    setGameOver(true);
   };
 
-  const moveSnake = () => {
-
-  };
+  const moveSnake = ({ keyCode }) => ( // только стрелки
+    keyCode >= 37 && keyCode <= 40 && setDir(DIRECTIONS[keyCode])
+  );
 
   const newApple = () => {
 
@@ -34,22 +39,39 @@ const App = () => {
 
   };
 
-  const getBad = () => {
-
-  };
-
-  const gameLoop = () => {
-
-  };
+  // врезулись в границу?
+  const isBorder = (piece, snk = snake) => (
+    piece[0] * SCALE >= CANVAS_SIZE[0]
+      || piece[1] * SCALE >= CANVAS_SIZE[1]
+      || piece[0] < 0 || piece[1] < 0
+  );
 
   useEffect(() => {
-
+    const context = canvasRef.current.getContext('2d');
+    context.setTransform(SCALE, 0, 0, SCALE, 0, 0);
+    context.clearRect(0, 0, CANVAS_SIZE[0], CANVAS_SIZE[1]);
+    context.fillStyle = 'pink';
+    snake.forEach(([x, y]) => context.fillRect(x, y, 1, 1));
+    context.fillStyle = 'blue';
+    context.fillRect(apple[0], apple[1], 1, 1);
   }, [snake, apple, gameOver]);
+
+  // шаг игры
+  const gameLoop = () => {
+    const snakeCopy = JSON.parse(JSON.stringify(snake));
+    const newSnakeHead = [snakeCopy[0][0] + dir[0], snakeCopy[0][1] + dir[1]];
+    if (isBorder(newSnakeHead)) endGame();
+    snakeCopy.unshift(newSnakeHead); // добавить голову
+    snakeCopy.pop(); // удалить хвост
+    setSnake(snakeCopy);
+  };
+
+  useInterval(() => gameLoop(), speed);
 
   return (
     <div role="button" tabIndex="0" onKeyDown={(e) => moveSnake(e)}>
       <canvas
-        style={{border: "1px solid grey"}}
+        style={{ border: '1px solid grey' }}
         ref={canvasRef}
         width={`${CANVAS_SIZE[0]}px`}
         height={`${CANVAS_SIZE[1]}px`}
